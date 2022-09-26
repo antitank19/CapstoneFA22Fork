a368438796c60d1f9dc0f89c945d40d53fd672fb
antitank19 <trankhaiminhkhoi10a3@gmail.com>
2022-09-26 10:08:26 +0700

    Add OData Package

--- API/Mapping/AutoMapper.cs
+++ API/Mapping/AutoMapper.cs
@@ -9,21 +9,39 @@ public class AutoMapper : Profile
     public AutoMapper()
     {
         // Basic DTOs
-        CreateMap<Building, BuildingDto>().ReverseMap();
-        CreateMap<City, CityDto>().ReverseMap();
-        CreateMap<ContractHistory, ContractHistoryDto>().ReverseMap();
-        CreateMap<Contract, ContractDto>().ReverseMap();
-        CreateMap<District, DistrictDto>().ReverseMap();
-        CreateMap<Owner, OwnerDto>().ReverseMap();
-        CreateMap<Payment, PaymentDto>().ReverseMap();
-        CreateMap<PaymentType, PaymentTypeDto>().ReverseMap();
-        CreateMap<RentEntity, RentEntityDto>().ReverseMap();
-        CreateMap<Role, RoleDto>().ReverseMap();
-        CreateMap<Room, RoomDto>().ReverseMap();
-        CreateMap<RoomType, RoomTypeDto>().ReverseMap();
-        CreateMap<Transaction, TransactionDto>().ReverseMap();
-        CreateMap<University, UniversityDto>().ReverseMap();
-        CreateMap<User, UserDto>().ReverseMap();
-        CreateMap<Ward, WardDto>().ReverseMap();
+        CreateMap<Building, BuildingDto>()
+            .ReverseMap();
+        CreateMap<City, CityDto>()
+            .ReverseMap();
+        CreateMap<ContractHistory, ContractHistoryDto>()
+            .ReverseMap();
+        CreateMap<Contract, ContractDto>()
+            .ReverseMap();
+        CreateMap<District, DistrictDto>()
+            .ReverseMap();
+        CreateMap<Owner, OwnerDto>()
+            .ReverseMap();
+        CreateMap<Payment, PaymentDto>()
+            .ReverseMap();
+        CreateMap<PaymentType, PaymentTypeDto>()
+            .ReverseMap();
+        CreateMap<RentEntity, RentEntityDto>()
+            .ReverseMap();
+        CreateMap<Role, RoleDto>()
+            .ReverseMap();
+        CreateMap<Room, RoomDto>()
+            .ReverseMap();
+        CreateMap<RoomType, RoomTypeDto>()
+            .ReverseMap();
+        CreateMap<Transaction, TransactionDto>()
+            .ReverseMap();
+        CreateMap<University, UniversityDto>()
+            .ReverseMap();
+        CreateMap<User, UserDto>()
+            .ReverseMap()
+            // For Odata Explicit Expansion
+            .ForAllMembers(o => o.ExplicitExpansion());
+        CreateMap<Ward, WardDto>()
+            .ReverseMap();
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.OData;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
 using Utilities.MiddlewareExtension;
@@ -6,8 +7,12 @@ using Utilities.ServiceExtensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+/*
 builder.Services.AddControllers();
+*/
+//Add odata to api
+builder.Services.AddControllers()
+    .AddOData(o=>o.EnableQueryFeatures());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
