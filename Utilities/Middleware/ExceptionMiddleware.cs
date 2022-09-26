b5c2ec1dd58b9f0e39019d587f49e08cf9b9c056
OliviaAlter <Innocentbystander0123@gmail.com>
2022-09-26 19:17:07 +0900

    Updated middleware's logging

--- Utilities/Middleware/ExceptionMiddleware.cs
+++ Utilities/Middleware/ExceptionMiddleware.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
+using Microsoft.IO;
 using Microsoft.Net.Http.Headers;
 
 namespace Utilities.Middleware;
@@ -19,12 +19,15 @@ public class ExceptionMiddleware
     private readonly IActionResultExecutor<ObjectResult> _executor;
     private readonly ILogger<ExceptionMiddleware> _logger;
     private static readonly ActionDescriptor EmptyActionDescriptor = new();
+    private readonly RecyclableMemoryStreamManager _recyclableMemoryStreamManager;
+    private const int ReadChunkBufferLength = 4096;
 
-    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IActionResultExecutor<ObjectResult> executor)
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IActionResultExecutor<ObjectResult> executor, RecyclableMemoryStreamManager recyclableMemoryStreamManager)
     {
         _next = next;
         _logger = logger;
         _executor = executor;
+        _recyclableMemoryStreamManager = new RecyclableMemoryStreamManager();
     }
 
     public async Task Invoke(HttpContext context)
@@ -33,28 +36,15 @@ public class ExceptionMiddleware
         var requestDisplayUrl = context.Request.GetDisplayUrl();
         try
         {
-            //Copy pointer to the original response body stream
-            var originalBodyStream = context.Response.Body;
-
-            //Get incoming request
-            var request = await GetRequestAsTextAsync(context.Request);
-            
-            //Create a new memory stream and use it for the temp response body
-            await using var responseBody = new MemoryStream();
-            context.Response.Body = responseBody;
-            
-            await _next(context);
-            var response = await GetResponseAsTextAsync(context.Response);
-            _logger.LogInformation("Request url: {RequestDisplayUrl}." +
-                                   "\nRequest data: {RequestData}." +
-                                   "\nRequest : {Request}." +
-                                   "\nResponse : {Response}",requestDisplayUrl, requestData, request, response);
-            await responseBody.CopyToAsync(originalBodyStream);
+            await LogRequest(context.Request);
+            await LogResponseAsync(context);
         }
         catch (Exception ex)
         {
             _logger.LogError(ex, 
-                "An unhandled exception has occurred while executing the request. Url: {RequestDisplayUrl}. Request Data: {RequestData}",requestDisplayUrl, requestData);
+                "An unhandled exception has occurred while executing the request. " +
+                "\nUrl: {RequestDisplayUrl}. " +
+                "\nRequest Data: {RequestData}",requestDisplayUrl, requestData);
             
             var routeData = context.GetRouteData();
 
@@ -67,7 +57,8 @@ public class ExceptionMiddleware
                 StatusCode = (int) HttpStatusCode.InternalServerError,
             };
 
-            await _executor.ExecuteAsync(actionContext, result);        }
+            await _executor.ExecuteAsync(actionContext, result);        
+        }
     }
     
     private static string GetRequestData(HttpContext context)
@@ -96,50 +87,71 @@ public class ExceptionMiddleware
         response.Headers.Remove(HeaderNames.ETag);
     }
     
-    private static async Task<string> GetRequestAsTextAsync(HttpRequest request)
+    private async Task LogRequest(HttpRequest request)
     {
-        var body = request.Body;
-
-        //Set the reader for the request back at the beginning of its stream.
         request.EnableBuffering();
+        await using var requestStream = _recyclableMemoryStreamManager.GetStream();
+        await request.Body.CopyToAsync(requestStream);
+        request.Body.Position = 0;
+        
+        var queryStringNullOrNot = request.QueryString.HasValue ? request.QueryString.Value : "No query string";
 
-        // Convert request content to string value to avoid boxing from long to object
-        var requestContent = request.ContentLength is > 0
-            ? await new StreamReader(request.Body).ReadToEndAsync()
-            : string.Empty;
-
-        if (requestContent.IsNullOrEmpty()) 
-            return "No request body is found";
-
-        //Convert to byteArray 
-        var byteArray = Convert.ToInt32(requestContent);
-
-        //Read request stream
-        var buffer = new byte[byteArray];
-
-        //Copy into buffer.
-        await request.Body.ReadAsync(buffer, 0, buffer.Length);
-
-        //Convert the byte[] into a string using UTF8 encoding...
-        var bodyAsText = Encoding.UTF8.GetString(buffer);
-
-        //Assign the read body back to the request body
-        request.Body = body;
-
-        return $"Request Scheme : {request.Scheme}," +
-               $"\nRequest Host : {request.Host}," +
-               $"\nRequest Path : {request.Path}," +
-               $"\nRequest Query String {request.QueryString}," +
-               $"\nRequest Body : {bodyAsText}";
+        var readStreamInChunks = await ReadStreamInChunks(requestStream);
+        
+        _logger.LogInformation("\nHttp Request Information:" +
+                               "\nSchema:{request.Scheme} " + 
+                               "\nHost: {request.Host} " +
+                               "\nPath: {request.Path} " +
+                               "\nQueryString: {request.QueryString} " +
+                               "\nRequest Body: {ReadStreamInChunks} "
+            , request.Scheme, request.Host, 
+            request.Path, queryStringNullOrNot, readStreamInChunks);
     }
 
-    private async Task<string> GetResponseAsTextAsync(HttpResponse response)
+    private async Task LogResponseAsync(HttpContext context)
     {
-        response.Body.Seek(0, SeekOrigin.Begin);
-        //Create stream reader to write entire stream
-        var text = await new StreamReader(response.Body).ReadToEndAsync();
-        response.Body.Seek(0, SeekOrigin.Begin);
+        var originalBody = context.Response.Body;
+        await using (var responseStream = _recyclableMemoryStreamManager.GetStream())
+        {
+            context.Response.Body = responseStream;
+            await _next.Invoke(context);
+            await responseStream.CopyToAsync(originalBody);
+            
+            var readStreamInChunks = await ReadStreamInChunks(responseStream);
+            
+            var queryStringNullOrNot = context.Request.QueryString.HasValue ? context.Request.QueryString.Value : "No query string";
 
-        return text;
+            _logger.LogInformation("\nHttp Response Information\n" +
+                                   "\nSchema:{context.Request.Scheme} " +
+                                   "\nHost: {context.Request.Host} " +
+                                   "\nPath: {context.Request.Path} " +
+                                   "\nQueryString: {context.Request.QueryString} " +
+                                   "\nResponse Body: {ReadStreamInChunks}",
+                    context.Request.Scheme, context.Request.Host, 
+                    context.Request.Path, queryStringNullOrNot , readStreamInChunks);
+        }
+
+        context.Response.Body = originalBody;
     }
+    
+
+    private static async Task<string> ReadStreamInChunks(Stream stream)
+    {
+        stream.Seek(0, SeekOrigin.Begin);
+        await using var textWriter = new StringWriter();
+        using var reader = new StreamReader(stream);
+        var readChunk = new char[ReadChunkBufferLength];
+        int readChunkLength;
+        //do while: is useful for the last iteration in case readChunkLength < chunkLength
+        do
+        {
+            readChunkLength = await reader.ReadBlockAsync(readChunk, 0, ReadChunkBufferLength);
+            await textWriter.WriteAsync(readChunk, 0, readChunkLength);
+        } while (readChunkLength > 0);
+
+        var result = textWriter.ToString();
+
+        return result;
+    }
+
 }
--- Utilities/ServiceExtensions/ScopedService.cs
+++ Utilities/ServiceExtensions/ScopedService.cs
@@ -1,6 +1,7 @@
 using Application.IRepository;
 using Application.Repository;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IO;
 using Service.IService;
 using Service.Service;
 
@@ -10,55 +11,7 @@ public static class ScopedService
 {
     public static IServiceCollection AddScopedService(this IServiceCollection services)
     {
-        /*
-        services.AddScoped<IBuildingService, BuildingService>();
-        services.AddScoped<IBuildingRepository, BuildingRepository>();
-
-        services.AddScoped<ICityService, CityService>();
-        services.AddScoped<ICityRepository, CityRepository>();
-
-        services.AddScoped<IContractHistoryService, ContractHistoryService>();
-        services.AddScoped<IContractHistoryRepository, ContractHistoryRepository>();
-
-        services.AddScoped<IContractService, ContractService>();
-        services.AddScoped<IContractRepository, ContractRepository>();
-
-        services.AddScoped<IDistrictService, DistrictService>();
-        services.AddScoped<IDistrictRepository, DistrictRepository>();
-
-        services.AddScoped<IOwnerService, OwnerService>();
-        services.AddScoped<IOwnerRepository, OwnerRepository>();
-
-        services.AddScoped<IPaymentService, PaymentService>();
-        services.AddScoped<IPaymentRepository, PaymentRepository>();
-        
-        services.AddScoped<IPaymentTypeService, PaymentTypeService>();
-        services.AddScoped<IPaymentTypeRepository, PaymentTypeRepository>();
-
-        services.AddScoped<IRentEntityService, RentEntityService>();
-        services.AddScoped<IRentEntityRepository, RentEntityRepository>();
-
-        services.AddScoped<IRoleService, RoleService>();
-        services.AddScoped<IRoleRepository, RoleRepository>();
-
-        services.AddScoped<IRoomService, RoomService>();
-        services.AddScoped<IRoomRepository, RoomRepository>();
-        
-        services.AddScoped<IRoomTypeService, RoomTypeService>();
-        services.AddScoped<IRoomTypeRepository, RoomTypeRepository>();
-        
-        services.AddScoped<ITransactionService, TransactionService>();
-        services.AddScoped<ITransactionRepository, TransactionRepository>();
-        
-        services.AddScoped<IUniversityService, UniversityService>();
-        services.AddScoped<IUniversityRepository, UniversityRepository>();
-        
-        services.AddScoped<IUserService, UserService>();
-        services.AddScoped<IUserRepository, UserRepository>();
-
-        services.AddScoped<IWardService, WardService>();
-        services.AddScoped<IWardRepository, WardRepository>();
-        */
+        services.AddSingleton<RecyclableMemoryStreamManager>();
         services.AddScoped<IServiceWrapper, ServiceWrapper>();
         services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
 
